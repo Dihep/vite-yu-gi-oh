28ccc22639d141e846cd554e77200459c0b26747
Dihep <dihep.demayo@gmail.com>
2024-06-02 21:09:28 +0200

    M1- Base pagina senza componenti.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,40 @@ import PlaceholderComponent from './components/PlaceholderComponent.vue'
 </script>
 
 <template>
+
+  <header>
+    <div>Yu-Gi-Oh API</div>
+  </header>
+
+  <main>
+    <select name="Type" id="cardsType">
+      <option value="alien">Alien</option>
+    </select>
+    <div id="cardsDisplay">
+      content
+    </div>
+  </main>
  
 </template>
 
 <style scoped>
-
+  header div {
+    width: 100%;
+    background-color: white;
+    color: black;
+    padding: 1rem 10vw;
+    font-size: 2rem;
+  }
+  main {
+    padding: 2rem 10vw;
+    background-color: orange;
+  }
+  select {
+    margin-bottom: 1rem;
+  }
+  #cardsDisplay {
+    background-color: white;
+    padding: 1.5rem;
+    color: black;
+  }
 </style>
